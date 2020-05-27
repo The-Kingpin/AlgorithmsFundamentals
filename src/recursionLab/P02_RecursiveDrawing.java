2e7470d1f43f01fdd2296aa3bc71699b542106ed
d3v1lkin <svetoslav.ivanov@windowslive.com>
2020-05-27 18:49:10 +0300

    Add solution Recusive Drawing

---
+++ src/recursionLab/P02_RecursiveDrawing.java
@@ -0,0 +1,49 @@
+package recursionLab;
+
+import java.util.Scanner;
+
+public class P02_RecursiveDrawing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        recursiveDrawing(num);
+    }
+
+    public static void recursiveDrawing(int num) {
+        drawStars(num);
+        drawHashTag(1, num);
+    }
+
+    public static void drawStars(int num) {
+        if (num > 1) {
+            for (int i = 0; i < num; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            drawStars(num - 1);
+
+        } else {
+            System.out.println("*");
+        }
+    }
+
+    public static void drawHashTag(int start, int end) {
+
+        if (start < end) {
+            for (int i = 0; i < start; i++) {
+                System.out.print("#");
+            }
+
+            System.out.println();
+
+            drawHashTag(start + 1, end);
+        }else {
+            for (int i = 0; i <end; i++) {
+                System.out.print("#");
+            }
+
+        }
+    }
+}
